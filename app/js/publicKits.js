697e11ac43ba838a7dcbb46d8de1e6209723481c
Alexandre <kavalerskialexandre@gmail.com>
2021-03-11 12:59:36 -0300

    Refatorando arquitetura do projeto

---
+++ app/js/publicKits.js
@@ -0,0 +1,59 @@
+const kits = compartilhador.getAllKits();
+
+kits.then((kits) => {
+  generateMarkupCustomKits(customKits);
+});
+
+function generateMarkupCustomKits(customKits) {
+  let markup = ``;
+  for (const kit of customKits) {
+    markup += `<div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">${kit.title}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${renderType(
+              kit.type
+            )}</h6>
+            <p class="card-text">${kit.description}</p>
+            <a class="btn btn-primary btnDisponibilizar" id="${
+              kit.id
+            }">Importar</a>
+          </div>
+        </div>`;
+  }
+
+  addMarkup(markup);
+}
+
+function addMarkup(markup) {
+  const container = document.getElementById("listKits");
+  container.innerHTML = markup;
+  addListenerMakeAvailable();
+}
+
+function addListenerMakeAvailable() {
+  Array.from(document.getElementsByClassName("btnDisponibilizar")).forEach(
+    function (element) {
+      element.addEventListener("click", (event) => {
+        compartilhador
+          .makeKitPublic(event.target, kits)
+          .then((kitSelecionado) => {
+            alert(
+              `o kit '${kitSelecionado.title}' criado por você agora está disponível para tds!`
+            );
+          });
+      });
+    }
+  );
+}
+
+function renderType(type) {
+  return type == "METHOD"
+    ? "Método de Trabalho"
+    : type == "LEARNING"
+    ? "Tópico de aprendizagem"
+    : type == "SYSTEM"
+    ? "Sistema de Tecnologia"
+    : type == "MODEL"
+    ? "Modelo de negócio"
+    : "Indicadores de Performance";
+}
