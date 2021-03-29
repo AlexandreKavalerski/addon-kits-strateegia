54280936186ba7d7a638e0a6b4b23867f5b7e631
Filipe Calegario <fcac@cin.ufpe.br>
2021-03-29 20:57:16 -0300

    Fix to entangled kits and questions: unique ids

--- app/js/visualData.js
+++ app/js/visualData.js
@@ -80,7 +80,7 @@ function drawProject(projectId) {
             }
             // console.log(missionId + " -> " + missionTitle);
             getAllContentsByMissionId(access_token, missionId).then(response => {
-                // console.log(response);
+                console.log(response);
                 let arrayContents = response.content;
                 for (let i = 0; i < arrayContents.length; i++) {
                     const contentId = arrayContents[i].id;
@@ -88,15 +88,15 @@ function drawProject(projectId) {
                     const kitId = arrayContents[i].kit.id;
                     const kitTitle = arrayContents[i].kit.title;
                     const kitCreatedAt = arrayContents[i].kit.created_at;
-                    addNode(kitId, kitTitle, "ferramentas", kitCreatedAt);
-                    addLink(missionId, kitId);
+                    addNode(contentId, kitTitle, "ferramentas", kitCreatedAt);
+                    addLink(missionId, contentId);
                     const arrayQuestions = arrayContents[i].kit.questions;
                     for (let j = 0; j < arrayQuestions.length; j++) {
-                        const questionId = arrayQuestions[j].id;
+                        const questionId = `${arrayQuestions[j].id}.${contentId}`;
                         const questionText = arrayQuestions[j].question;
                         const questionCreatedAt = arrayQuestions[j].created_at;
                         addNode(questionId, questionText, "questões", questionCreatedAt);
-                        addLink(kitId, questionId);
+                        addLink(contentId, questionId);
                         getParentComments(access_token, contentId, questionId).then(response => {
                             // console.log(response);
                             let arrayComments = response.content;
